bd85c8e9e998921db1e544f369b6dac55ce2900b
bkuste2 <igorribeiro632@gmail.com>
2023-07-26 13:59:37 -0300

    :test_tube: hotfix deploy

--- src/components/sign-in-form/index.tsx
+++ src/components/sign-in-form/index.tsx
@@ -5,7 +5,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useState } from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
 import { z } from 'zod'
-import { Form } from '../forme'
+import { Form } from '../form'
 
 export function SignInForm() {
   const [loading, setLoading] = useState(false)
